5a8a5d92a3e6d2b937cd1cff4a8913876401bc8a
Muhammad Irfan <irfan.sfdev@gmail.com>
2026-07-23 09:09:33 +0500

    task1 and 2 complete in basic practice

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+// app/about/page.tsx
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-3xl mt-10">About Us</h1>
+      <p>This is the about page. </p>
+    </div>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+// app/contact/page.tsx
+export default function Contact() {
+  return (
+    <div>
+      <h1 className="text-3xl mt-10">Contact Us</h1>
+      <p>This is the Contact page</p>
+    </div>
+  );
+}
